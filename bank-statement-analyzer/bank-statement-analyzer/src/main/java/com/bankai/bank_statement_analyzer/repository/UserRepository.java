16ec385d4e796f5908c3a4659c1876473b86fdea
Diyali14 <diyali14082004@gmailcom>
2026-05-23 14:21:56 +0530

    Altered application.property file and first entity , repo layer , db integration done .

---
+++ bank-statement-analyzer/bank-statement-analyzer/src/main/java/com/bankai/bank_statement_analyzer/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.bankai.bank_statement_analyzer.repository;
+
+import com.bankai.bank_statement_analyzer.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
